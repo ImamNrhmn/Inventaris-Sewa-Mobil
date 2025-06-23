9505893eff30cda9c7e09148230762953ff4749e
ImamNrhmn <c030324038@mahasiswa.poliban.ac.id>
2025-06-23 19:46:22 +0700

    Menambahkan struct mobil

--- sewaMobil.cpp
+++ sewaMobil.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <conio.h>
+#include <string>
 using namespace std;
+
+struct Mobil {
+    int id;
+    string merk;
+    string tipe;
+    int tahun;
+    bool disewa;
+    Mobil* next;
+};
 
 int main() {
     cout << "Hello, world!" << endl;
