bfe6b08d0fc6c1ce4a10c189958b8719ad9dc8ff
ImamNrhmn <c030324038@mahasiswa.poliban.ac.id>
2025-06-23 19:49:17 +0700

    Menambahkan fungsi tampilan header menu dan garis otomatis

--- sewaMobil.cpp
+++ sewaMobil.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 struct Mobil {
@@ -11,6 +12,20 @@ struct Mobil {
     bool disewa;
     Mobil* next;
 };
+
+// Fungsi untuk membuat garis menu
+void garis(int n = 50) {
+    for (int i = 0; i < n; ++i) cout << "=";
+    cout << endl;
+}
+
+// Fungsi untuk menampilkan header menu
+void headerMenu() {
+    system("cls");
+    garis();
+    cout << setw(30) << "INVENTARIS SEWA MOBIL" << endl;
+    garis();
+}
 
 int main() {
     cout << "Hello, world!" << endl;
